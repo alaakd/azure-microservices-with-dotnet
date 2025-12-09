82c1dcb56691d213878c7bf8e805f013a153bdda
alaakd <kirdi.alaa@gmail.com>
2025-12-09 17:39:15 -0500

    chapter 2 complete

---
+++ src/Wpm.Management.Api/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Api.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ManagementDbContext>(options =>
+{
+    options.UseInMemoryDatabase("WpmManagement");
+});
+
+var app = builder.Build();
+
+app.EnsureDbIsCreated();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
